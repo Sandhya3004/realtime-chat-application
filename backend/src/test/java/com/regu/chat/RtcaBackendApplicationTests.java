e7958b192ae806e2b5e49c066384eda74c388d97
Sandhya3004 <23eg105v12@anurag.edu.in>
2026-02-25 16:29:32 +0000

    fix🔧: enable CORS

---
+++ backend/src/test/java/com/regu/chat/RtcaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regu.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
